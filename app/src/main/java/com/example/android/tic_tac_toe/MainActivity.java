9205bf62f4f251eb9545a05026b2c75b073c87a4
chengzheng-wang <cwang92@binghamton.edu>
2017-07-14 13:33:24 -0700

    yello and then red logic

--- app/src/main/java/com/example/android/tic_tac_toe/MainActivity.java
+++ app/src/main/java/com/example/android/tic_tac_toe/MainActivity.java
@@ -4,14 +4,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    boolean flag = true;
     public void drop (View view) {
 
         ImageView counter = (ImageView)view;
         counter.setTranslationY(-1000f);
         counter.setTranslationX(-1000f);
-        counter.setImageResource(R.drawable.yellow);
+
+//        int height = counter.getLayoutParams().height;
+//        int width = counter.getLayoutParams().width;
+//        String str = height + " " + width;
+//        Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+
+        if (flag) {
+            counter.setImageResource(R.drawable.yellow);
+            flag = false;
+        } else {
+            counter.setImageResource(R.drawable.red);
+            flag = true;
+        }
+
         counter.animate().translationYBy(1000f).translationXBy(1000f).setDuration(300);
     }
 
